dfaaf669a7cd9d4c81ea20260e40e65932938ab4
ily-e <ily-e@yandex.ru>
2022-03-15 15:17:42 +0300

    Initial commit

---
+++ userBrokenJobs/src/UserBrokenJobs.java
@@ -0,0 +1,79 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class UserBrokenJobs {
+
+    private static String mailFrom;
+    private static String[] mailTo;
+    private static String mailPass;
+
+    private static String warningJobs = "";
+
+    public static void main(String[] args) throws Exception {
+
+        searchFreeSpace(args[0],args[1],args[2],args[3],args[4]);
+
+        if (warningJobs.length() > 1){
+            mailFrom = args[5];
+            mailPass = args[6];
+            mailTo = new String[args.length-7];
+            for (int i = 0; i < args.length-7; i++){
+                mailTo[i] = args[i+7];
+            }
+        }
+
+        sendMail("Внимание! Обнаружены ошибка в базе "+ args[2]+"!","Не исправные Job!\n" + warningJobs);
+
+    }
+
+    public static void searchFreeSpace(String BaseIp, String basePort, String baseName, String baseUser, String basePass) throws Exception {
+        Connection oraCon = OracleConnection.initConnect(BaseIp, basePort, baseName, baseUser, basePass);
+        try {
+            Statement statement = oraCon.createStatement();
+            ResultSet cFreeSpace = statement.executeQuery("" +
+                    "select t.rw\n" +
+                    "  from LIST_WARNING_DATABASE t");
+            while (cFreeSpace.next()) {
+                    warningJobs += cFreeSpace.getString("rw") + "\n";
+            }
+            cFreeSpace.close();
+            oraCon.close();
+        }
+        catch (Exception ex) {
+            //выводим наиболее значимые сообщения
+            Logger.getLogger(OracleConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally
+        {
+            if (oraCon != null) {
+                try {
+                    oraCon.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(OracleConnection.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+
+    public static void sendMail (String subject, String msg) throws Exception{
+
+        Mail mail = new Mail(mailFrom, mailPass);//"jqthrnkqngfyfpyw"//"ily-e@yandex.ru"
+        mail.setHostPostSport("smtp.yandex.ru","465","465");
+        mail.setFrom(mailFrom);//"ily-e@yandex.ru"
+        String[] to  = new String[1];;
+        to = mailTo;//"ily-e@mail.ru";
+        mail.setTo(to);
+        mail.setSubject(subject);
+        mail.setBody(msg);
+        mail.send();
+
+    }
+
+
+
+
+}
